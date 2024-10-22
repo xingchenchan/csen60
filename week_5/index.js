6cd2ca7a89cc80af4706aa1650e3f5bb34782040
xingchen33 <tippi2935480894@gmail.com>
2024-10-21 18:38:50 -0700

    monday

---
+++ week_5/index.js
@@ -0,0 +1,24 @@
+// console.log("hello world")
+// alert('Hello, world!');
+window.document.write('<p>Welcome to CSEN 60!</p>');
+
+// document.append('<p>This should come after the h1</p>')
+
+/*
+    Function to calculate the sum of two numbers.
+    Inputs: a, b - numbers to be added.
+    Output: returns the sum of a and b.
+*/
+
+// TODO: fix this function
+
+/**
+ * @description: adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @return {number} The sum of a and b.
+ */
+
+function add(a, b) {
+    return a + b;
+}
