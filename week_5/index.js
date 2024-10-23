009d63e8f3bd03f6d6ff63fd857b175e7af80bde
xingchen33 <tippi2935480894@gmail.com>
2024-10-23 11:34:51 -0700

    wed

--- week_5/index.js
+++ week_5/index.js
@@ -1,6 +1,6 @@
 // console.log("hello world")
 // alert('Hello, world!');
-window.document.write('<p>Welcome to CSEN 60!</p>');
+// window.document.write('<p>Welcome to CSEN 60!</p>');
 
 // document.append('<p>This should come after the h1</p>')
 
@@ -22,3 +22,50 @@ window.document.write('<p>Welcome to CSEN 60!</p>');
 function add(a, b) {
     return a + b;
 }
+
+let x = 100;
+if (true) {
+    x = 200;
+}
+console.log(x);
+
+let y
+y = null
+let z = 0
+console.log(y, z);
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date()}`;
+console.log(response)
+
+const age = 17
+
+if(age > 18){
+    console.log("You are of age")
+} else{
+    console.log(`You aren't of age.  Please wait ${18 - age} years`)
+}
+
+let isOverAge = (age > 18);
+
+console.log("\n", isOverAge)
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+person.age
+console.log(person["age"])
+
+console.log(typeof("hello world"))
+
+alert("This is  a important message")
+
+// Strict equality, 1: are these the same type, 2: are these the same value
+console.log(3 === '3'); // false
+console.log(3 === 3)
+
+// Loose equality
+console.log(3 == '3'); // true
