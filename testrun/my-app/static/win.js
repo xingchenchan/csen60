51c870bb32f0233e4292245e7b2626398b6f0878
xingchen33 <tippi2935480894@gmail.com>
2024-11-27 00:22:47 -0800

    awr

---
+++ testrun/my-app/static/win.js
@@ -0,0 +1,69 @@
+document.cookie = "name=Oeschger; SameSite=None; Secure";
+document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+
+function showCookies() {
+    const output = document.getElementById("cookies");
+    output.textContent = `> ${document.cookie}`;
+}
+
+function clearOutputCookies() {
+    const output = document.getElementById("cookies");
+    output.textContent = "";
+}
+
+
+// function setCookie() {
+//     document.cookie = "checked"
+//     let ca = document.cookie
+//     return ca
+// }
+
+// function getCookie() {
+//     let ca = document.cookie
+//     return ca
+// }
+
+// function checkCookie() {
+//     let user = getCookie()
+//     if (user != "") {
+//     alert("Welcome again " + user)
+// }
+// }
+
+
+
+// var x = 0
+
+// function hamu(){
+//     console.log("activated")
+
+
+// }
+
+// function changeglobal(){
+
+// }
+
+// function cookieread(){
+
+//     console.log(x)
+
+
+//     if (x == 1){
+
+//         console.log("passed")
+//         return execute()
+//         const img = document.createElement("img")
+//         img.src="./test.jpg"
+//         document.body.appendChild(img)
+
+//         console.log("haha")
+
+//     }
+
+// }
+
+
+
+
+
